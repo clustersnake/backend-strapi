adada45809665fbabe7dcae567d36f81e99abaa8
Eloy Ramirez <clustersnake45@gmail.com>
2024-02-15 14:56:20 -0400

    feat: Usado version 18 de node

--- src/api/estudiante/routes/estudiante.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * estudiante router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::estudiante.estudiante');
--- src/api/estudiante/services/estudiante.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * estudiante service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::estudiante.estudiante');
